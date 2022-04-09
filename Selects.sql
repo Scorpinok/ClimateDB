7d21c898c81cf80a847c7a2a539977f5fe1fd669
Scorpinok <scorpinok999@gmail.com>
2022-04-09 14:08:33 +0700

    Add files via upload

--- Selects.sql
+++ Selects.sql
@@ -15,7 +15,7 @@ select cs.source_name as "Источник данных", count(p.precipitation)
 from climatic_sources as cs, precipitations as p
 where cs.id_cl_source = p.id_cl_source and p.precipitation < 10
 group by cs.source_name
-order by "Количество" desc;
+order by count(p.precipitation) desc;
 
 
 -- Запрос выводит количество тестовых участков (ТУ) с широтой равной 10
@@ -25,4 +25,30 @@ where ts.id_ts = dd.id_ts and dd.id_dat_des in
 (select c.id_dat_des
 from latitudes as l, coordinates as c
 where l.id_lat = c.id_lat and l.`Latitude` = 10);
+
+
+-- Запрос выводит географические коорднаты для климатического показателя - давление
+select p.pressure as "Давление", lo.Longitude as "Долгота", la.Latitude as "Широта"
+from longitudes lo, latitudes la, coordinates c, pressure p 
+where c.id_long = lo.id_long and c.id_lat = la.id_lat and c.id_cl_source = p.id_cl_source; 
+
+
+-- Запрос выводит среднюю площадь озер для тестового участка (ТУ)
+select ts.id_ts as "Номер ТУ", avg(l.area) as "Средняя площадь озера"
+from lakes l, test_sites ts 
+where l.id_ts = ts.id_ts
+group by ts.id_ts
+order by avg(l.area) desc;
+
+
+-- Запрос выводит описание тестовых участков, сформированное не более пяти лет назад
+select ts.id_ts as "Номер ТУ", dd.Description as "Описание"
+from data_descriptions dd, test_sites ts 
+where dd.id_ts = ts.id_ts and TIMESTAMPDIFF(YEAR, dd.Data_date, NOW()) < 5;
+
+
+-- Запрос выводит значения осадков для 1991 года
+select p.precipitation  as "Осадки"
+from years y, precipitations p
+where y.id_years = p.id_precip and y.`year` = 1991;
 
